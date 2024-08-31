9496ab172dc1dc696f117f35e373c0b3f7cc0c0f
Zack Njine Wachira <zackwachira45@gmail.com>
2024-08-31 19:05:53 +0300

    Initial commit

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+
+	fib_seq = []
+	if length > 0:
+		fib_seq.append(0)
+	if length >= 2:
+		fib_seq.append(1)
+		for i in range(2, length):
+			fib_seq.append(fib_seq[-1] + fib_seq[-2])
+
+	print(fib_seq)
